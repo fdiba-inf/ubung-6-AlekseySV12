52a690d17d3c82574624410ba6b94bdd7e9ada66
AlekseySV12 <alekseysv@abv.bg>
2020-11-04 09:35:52 +0000

    .

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -4,24 +4,26 @@ import java.util.Scanner;
 
 public class PasswordCheck {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String password = input.nextLine();
-        char [] array= new char [password.length()];
-        boolean a=false;
-        boolean b=false;
-        int count=0;
-        for(int i=0;i<password.length();i++){
-            array [i] = password.charAt(i);
-            a=(Character.isLetterOrDigit(array[i]));
-            if(b==(Character.isDigit(array[i]))){
-                count++;
+        boolean c=false;
+        while (c==false) {
+            Scanner input = new Scanner(System.in);
+            String password = input.nextLine();
+            char[] array = new char[password.length()];
+            boolean a = false;
+            boolean b = false;
+            int count = 0;
+            for (int i = 0; i < password.length(); i++) {
+                array[i] = password.charAt(i);
+                a = (Character.isLetterOrDigit(array[i]));
+                if (b == (Character.isDigit(array[i]))) {
+                    count++;
+                }
+            }
+
+            if (password.length() >= 8 && a == true && count >= 2) {
+                System.out.println("Password valid!");
+                c=true;
             }
         }
-
-        if(password.length()>=8 && a==true && count>=2)
-        {
-            System.out.println("Password valid!");
-        }
-
     }
 }
