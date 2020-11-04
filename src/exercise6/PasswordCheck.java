0848ead07c10bf898b668e04a1624684544c79c6
AlekseySV12 <alekseysv@abv.bg>
2020-11-04 17:03:49 +0000

    .

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -1,30 +1,52 @@
 package exercise6;
 
-import java.util.*;
+import java.util.Scanner;
+import java.lang.Character; 
+import java.util.Arrays;
 
 public class PasswordCheck {
     public static void main(String[] args) {
-        boolean c=false;
-        while (!c) {
-            Scanner input = new Scanner(System.in);
-            String password = input.nextLine();
-            char[] array = password.toCharArray();
-            boolean a = false;
-            boolean b = false;
-            int count = 0;
-            for (int i = 0; i < password.length(); i++)
-            {
-                a = (Character.isLetterOrDigit(array[i]));
-                if (Character.isDigit(array[i])) {
-                    count++;
+       Scanner input = new Scanner(System.in);
+
+        boolean lngth;
+        boolean cnt;
+        boolean a=false;
+
+        while(!a)
+        {
+        System.out.println("Enter a password: ");
+        String password = input.next();
+        int passwordLength = password.length();
+
+            if (passwordLength < 8) {
+                lngth=false;}
+            else  {lngth = true;}
+
+            char[] ch = password.toCharArray();
+            int counter1 = 0;
+            int counter2 = 0;
+            for (int i = 0; i < ch.length; i++) {
+                if (!Character.isLetterOrDigit(ch[i])) {
+                    counter1++;
+                }
+            }
+            for (int i = 0; i < ch.length; i++) {
+                if (Character.isDigit(ch[i])) {
+                    counter2++;
                 }
             }
 
-            if (password.length() >= 8 && a && count >= 2) {
-                System.out.println("Password valid!");
-                c=true;
-
+            if (counter1 > 1 || counter2 < 2) {
+                cnt = false;
+            } else {
+                cnt = true;
             }
+
+
+            if (cnt && lngth) {
+                System.out.println("Password valid!");
+                a=true;
+            }}
         }
+
     }
-}
